b54211246b63601a39bd8a2f04eff73b9fafd746
stereotype13 <stereotype13@hotmail.com>
2014-01-09 21:02:59 -0800

    Updated styling and added deletion

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -9,17 +9,17 @@ class CommentsController < ApplicationController
   	@comment.post = @post
 	
     #create a @topic variable so we know build the propper [@topic, @post] url to redirect to
-	@topic = Topic.find(params[:topic_id])
-	
-	authorize! :create, @comment, message: "You need to be a member to do that."
-	
-	if @comment.save
-		flash[:notice] = "Comment was saved."
-	else
-		flash[:error] = "There was an error saving the comment. Please try again."
-	end
-	 
-	redirect_to [@topic, @post] #builds the propper nested url 
+  	@topic = Topic.find(params[:topic_id])
+  	
+  	authorize! :create, @comment, message: "You need to be a member to do that."
+  	
+  	if @comment.save
+  		flash[:notice] = "Comment was saved."
+  	else
+  		flash[:error] = "There was an error saving the comment. Please try again."
+  	end
+  	 
+  	redirect_to [@topic, @post] #builds the propper nested url 
 	 
   end
 
@@ -34,5 +34,18 @@ class CommentsController < ApplicationController
   end
 
   def destroy
+    @topic = Topic.find(params[:topic_id])
+    @post = @topic.posts.find(params[:post_id])
+
+    @comment = @post.comments.find(params[:id])
+
+    authorize! :destroy, @comment, message: "You need to own the comment to delete it."
+    if @comment.destroy
+      flash[:notice] = "Comment was removed."
+      redirect_to [@topic, @post]
+    else
+      flash[:error] = "Comment couldn't be deleted. Try again."
+      redirect_to [@topic, @post]
+    end
   end
 end
--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -40,4 +40,19 @@ class TopicsController < ApplicationController
       render :edit
     end
   end
+
+  def destroy
+    @topic = Topic.find(params[:id])
+    name = @topic.name
+
+    authorize! :destroy, @topic, message: "You need to own the topic to delete it."
+
+    if @topic.destroy
+      flash[:notice] = "\"#{name}\" was deleted successfully."
+      redirect_to topics_path
+    else
+      flash[:error] = "There was an error deletingthe topic."
+      reder :show
+    end
+  end
 end
