c0aea97bcdc60bdfa1ba326a4453cfc5248efd95
stereotype13 <stereotype13@hotmail.com>
2014-01-14 20:43:59 -0800

    Implemented favoriting

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,18 +6,21 @@ class User < ActiveRecord::Base
          :omniauthable, :omniauth_providers => [:facebook]
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :name, :avatar, :user_id, :provider, :uid
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :name, :avatar, :user_id, 
+  :provider, :uid, :email_favorites
   # attr_accessible :title, :body
 
   has_many :posts
   has_many :comments
   has_many :votes, dependent: :destroy
+  has_many :favorites, dependent: :destroy
   
   before_create :set_member
 
   mount_uploader :avatar, AvatarUploader # add this line.
 
   def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
+    p auth
     user = User.where(:provider => auth.provider, :uid => auth.uid).first
     unless user
       pass = Devise.friendly_token[0,20]
@@ -38,6 +41,10 @@ class User < ActiveRecord::Base
   def role?(base_role)
     role.nil? ? false : ROLES.index(base_role.to_s) <= ROLES.index(role)
   end  
+
+  def favorited(post)
+    self.favorites.where(post_id: post.id).first
+  end
 
   private
 
