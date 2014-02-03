16d2788911b8a1288f7b4e884cc9666a7002b9e8
stereotype13 <stereotype13@hotmail.com>
2014-02-02 17:21:00 -0800

    Updated application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -29,3 +29,29 @@ $(document).ready(function() {
   });
 });
 
+$(document).ready(
+    function()
+    {
+     
+        $(".blocmetrics").click(
+        function(event)
+        {
+            var clickedObject = $(event.target);
+            var obj_id = clickedObject.prop('id');
+            var obj_type = clickedObject.prop('type');
+            
+            alert("I'm here!");
+            
+            $.ajax(
+                {
+                  type: "POST",
+                  url: "http://stereotype13-blocmetrics.herokuapp.com/event/data",
+                  data: { 
+                            parameter_1: "Triggered by Bloccit",
+                            parameter_2: "World"
+                        }
+                }
+            )
+        });
+    });
+
